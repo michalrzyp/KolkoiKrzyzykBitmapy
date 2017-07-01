e3ba27a89ebf1e4993eddc5e31ccad9fdf21c4b6
Michal Rzyp <michalrzyp@gmail.com>
2017-07-01 16:29:35 +0200

    nowa wersja

--- source/main.cpp
+++ source/main.cpp
@@ -9,15 +9,21 @@ enum wynikGry
   remis,
   nierozstrzygniety
 };
+void WstawPrzyciski(HWND hwndMainWindow);
 bool isGameOn = false;
 bool isFirstPlayerTurn = true;
 CHAR Text[200];
+CHAR szText[200];
 int Array[9];// 0 puste  pole 1 pole X -1 pole O
 void drawX(HDC hdc, int x, int y);
 void drawO(HDC hdc, int x, int y);
+void drawButton(HDC hdc);
 void drawBoard(HDC hdc);
+void Result(HWND hwndDig, wynikGry a);
+wynikGry CheckResult();
 wynikGry a = nierozstrzygniety;
 HINSTANCE hInstance;
+HDC Okno;
 HBITMAP hBitmapGameBoard, hBitmapX, hBitmapO, hBitmapStart, hBitmapReset;
 WNDPROC wpOrgButtonProc;
 LRESULT CALLBACK ButtonWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -26,15 +32,7 @@ LRESULT CALLBACK ButtonWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
   {
   case WM_PAINT:
   {
-    /*//paint the window's client menu
-    HDC hDCBitmap;
-    HDC hdc = GetDC(hwnd);
-    hDCBitmap = CreateCompatibleDC(hdc);
-    SelectObject(hDCBitmap, hBitmapStart);
-    //StretchBlt(hdc, 0, 0, 400, 264, hDCBitmap, 0, 0, 500, 364, SRCCOPY);
-    BitBlt(hdc, 0, 0, 84, 75, hDCBitmap, 0, 0, SRCCOPY);
-    DeleteDC(hDCBitmap);
-    ReleaseDC(hwnd, hdc);*/
+    //paint the window's client menu
   }
   return 0;
   default:
@@ -50,7 +48,7 @@ INT_PTR CALLBACK DialogProc(HWND hwndDig, UINT uMsg, WPARAM wParam, LPARAM lPara
   {
   case WM_INITDIALOG:
   {
-    HWND hwndButton1 = GetDlgItem(hwndDig,IDC_BUTTON1);
+    HWND hwndButton1 = GetDlgItem(hwndDig, IDC_BUTTON1);
     WNDPROC wpOrgButtonProc = (WNDPROC)SetWindowLong(hwndButton1, -4, (LONG)ButtonWndProc);
     hBitmapGameBoard = LoadBitmap(hInstance, MAKEINTRESOURCE(105));
     hBitmapX = LoadBitmap(hInstance, MAKEINTRESOURCE(104));
@@ -58,51 +56,97 @@ INT_PTR CALLBACK DialogProc(HWND hwndDig, UINT uMsg, WPARAM wParam, LPARAM lPara
     hBitmapStart = LoadBitmap(hInstance, MAKEINTRESOURCE(107));
     hBitmapReset = LoadBitmap(hInstance, MAKEINTRESOURCE(106));
   }
-    return TRUE;
-    //GWL_WNDPROC -4
-  case WM_LBUTTONDOWN:
-  {
-    int x = LOWORD(lParam), y = HIWORD(lParam);
-    if ((x >= 200 && x <= 410) && (y >= 50 && y <= 200))
+  return TRUE;
+  //GWL_WNDPROC -4
+  case WM_COMMAND:
+    switch (HIWORD(wParam))
     {
-      int filedX = ((x - 200) / 70);
-      int filedY = ((y - 50) / 50);
-      if (Array[filedX * 3 + filedY] == 0)
+    case BN_CLICKED:
+      switch (LOWORD(wParam))
       {
-        x = filedX * 70 + 200;
-        y = filedY * 50 + 50;//x=((x-iMinBoardX/iWidthBorardX)*iWidthX+iMinBoardX+iWidthBoardX)
+      case IDC_BUTTON1: 
+      {
+        isGameOn=!isGameOn;
         HDC hdc = GetDC(hwndDig);
-        // HWND hCtl1 = GetDlgItem(hwndDig, IDC_RADIO1);
-        // HWND hCtl2 = GetDlgItem(hwndDig, IDC_RADIO2);
-       // HPEN hMyPen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
-       // SelectObject(hdc, hMyPen);
-        if (isFirstPlayerTurn == true)
+        drawButton(hdc);
+        if (isGameOn == false)
         {
-          drawX(hdc, x, y);
-          Array[3 * filedX + filedY] = 1;
-          // CheckRadioButton(hwndDig, IDC_RADIO1, IDC_RADIO2, IDC_RADIO2);
-          // EnableWindow(hCtl1, FALSE);
-          // EnableWindow(hCtl2, TRUE);
-        }
-        else
-        {
-          drawO(hdc, x, y);
-          Array[3 * filedX + filedY] = -1;
-          //  CheckRadioButton(hwndDig, IDC_RADIO1, IDC_RADIO2, IDC_RADIO1);
-           // EnableWindow(hCtl2, FALSE);
-           // EnableWindow(hCtl1, TRUE);
+          drawBoard(hdc);
+          for (int i = 0; i < 9; i++)
+            Array[i] = 0;
         }
         ReleaseDC(hwndDig, hdc);
-        isFirstPlayerTurn = !isFirstPlayerTurn;
+        return TRUE;
+      }
+        return TRUE;
+      }
+      return FALSE;
+    }
+    return FALSE;
+  case WM_LBUTTONDOWN:
+  {
+    if (isGameOn == true)
+    {
+      int x = LOWORD(lParam), y = HIWORD(lParam);
+      if ((x >= 0 && x <= 500) && (y >= 0 && y <= 364))
+      {
+        int filedX = ((x - 0) / 167);
+        int filedY = ((y - 0) / 122);
+        if (Array[filedX * 3 + filedY] == 0)
+        {
+          x = filedX * 167 + 0;
+          y = filedY * 122 + 0;//x=((x-iMinBoardX/iWidthBorardX)*iWidthX+iMinBoardX+iWidthBoardX)
+          HDC hdc = GetDC(hwndDig);
+          if (isFirstPlayerTurn == true)
+          {
+            drawX(hdc, x, y);
+            Array[3 * filedX + filedY] = 1;
+          }
+          else
+          {
+            drawO(hdc, x, y);
+            Array[3 * filedX + filedY] = -1;
+          }
+          ReleaseDC(hwndDig, hdc);
+          isFirstPlayerTurn = !isFirstPlayerTurn;
+        }
       }
     }
+    a = CheckResult();
+    Result(hwndDig, a);
   }
-    return TRUE;
+  //wsprintf(szText, "Kliknales myszka x=%d, y=%d", LOWORD(lParam), HIWORD(lParam));
+ // MessageBox(hwndDig, szText, TEXT("Klikniecie"), MB_OK);
+  return TRUE;
   case WM_PAINT:
   {
-    HDC hdc = GetDC(hwndDig);
-    //BitBlt(hdc, 0, 0, 485, 242, GetDC(0), 0, 0, SRCCOPY);
+    UpdateWindow(hwndDig);
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hwndDig, &ps);
     drawBoard(hdc);
+    isGameOn = false;
+    drawButton(hdc);
+    //SendDlgItemMessage(hwndDig, IDC_BUTTON1, BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hBitmapStart);
+    int x, y;
+    for (int i = 0; i < 9; i++)
+    {
+      switch (i / 3)
+      {
+      case 0: x = 0; break;
+      case 1: x = 167; break;
+      case 2: x = 334; break;
+      }
+      switch (i % 3)
+      {
+      case 0: y = 0; break;
+      case 1: y = 122; break;
+      case 2: y = 244; break;
+      }
+      if (Array[i] != 0)
+        Array[i] == 1 ? drawX(hdc, x, y) : drawO(hdc, x, y);
+    }
+    ReleaseDC(hwndDig, hdc);
+    EndPaint(hwndDig, &ps);
   }
   return 0;
   case WM_CLOSE:
@@ -123,6 +167,7 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevinstance, PSTR szCmdLIne,
   HWND hwndMainWindow = CreateDialog(hinstance, MAKEINTRESOURCE(IDD_MAINVIEW), NULL, DialogProc);
   ShowWindow(hwndMainWindow, iCmdShow);
   MSG msg = {};
+  Okno = GetDC(hwndMainWindow);
   while (GetMessage(&msg, NULL, 0, 0))
   {
     TranslateMessage(&msg);
@@ -133,59 +178,46 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevinstance, PSTR szCmdLIne,
 
 void drawX(HDC hdc, int x, int y)
 {
-  /*x += 35;
-  y += 25;
-  HPEN hMyPen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
-  SelectObject(hdc, hMyPen);
-  MoveToEx(hdc, x - 10, y - 10, NULL);
-  LineTo(hdc, x + 10, y + 10);
-  MoveToEx(hdc, x - 10, y + 10, NULL);
-  LineTo(hdc, x + 10, y - 10);*/
   HDC hDCBitmap;
   hDCBitmap = CreateCompatibleDC(hdc);
   SelectObject(hDCBitmap, hBitmapX);
-  //StretchBlt(hdc, 0, 0, 400, 264, hDCBitmap, 0, 0, 500, 364, SRCCOPY);
-  BitBlt(hdc, 0, 0, 84, 75, hDCBitmap, 0, 0, SRCCOPY);
+  BitBlt(hdc, x + 36, y + 5, 95, 110, hDCBitmap, 0, 0, SRCCOPY);
   DeleteDC(hDCBitmap);
 }
 void drawO(HDC hdc, int x, int y)
 {
-  /*x += 35;
-  y += 25;
-  HPEN hMyPen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
-  SelectObject(hdc, hMyPen);
-  MoveToEx(hdc, x - 10, y - 10, NULL);
-  LineTo(hdc, x - 10, y + 10);
-  LineTo(hdc, x - 10, y - 10);
-  LineTo(hdc, x + 10, y - 10);
-  LineTo(hdc, x + 10, y + 10);
-  LineTo(hdc, x - 10, y + 10);*/
   HDC hDCBitmap;
   hDCBitmap = CreateCompatibleDC(hdc);
   SelectObject(hDCBitmap, hBitmapO);
-  //StretchBlt(hdc, 0, 0, 400, 264, hDCBitmap, 0, 0, 500, 364, SRCCOPY);
-  BitBlt(hdc, 0, 0, 84, 75, hDCBitmap, 0, 0, SRCCOPY);
+  BitBlt(hdc, x + 27, y + 5, 112, 110, hDCBitmap, 0, 0, SRCCOPY);
   DeleteDC(hDCBitmap);
 }
 void drawBoard(HDC hdc)
 {
-  /*Rectangle(hdc, 200, 50, 410, 200);
-  MoveToEx(hdc, 200, 100, NULL);
-  LineTo(hdc, 410, 100);
-  MoveToEx(hdc, 200, 150, NULL);
-  LineTo(hdc, 410, 150);
-
-  MoveToEx(hdc, 270, 50, NULL);
-  LineTo(hdc, 270, 200);
-  MoveToEx(hdc, 340, 50, NULL);
-  LineTo(hdc, 340, 200);*/
   HDC hDCBitmap;
   hDCBitmap = CreateCompatibleDC(hdc);
   SelectObject(hDCBitmap, hBitmapGameBoard);
-  StretchBlt(hdc, 0, 0, 400, 264, hDCBitmap, 0, 0, 500,364, SRCCOPY);
+  StretchBlt(hdc, 0, 0, 500, 364, hDCBitmap, 0, 0, 500, 364, SRCCOPY);
+  drawButton(hdc);
   DeleteDC(hDCBitmap);
 }
-/*
+void drawButton(HDC hdc)
+{
+  HDC hDCBitmap;
+  hDCBitmap = CreateCompatibleDC(hdc);
+  if (isGameOn == false)
+  {
+    SelectObject(hDCBitmap, hBitmapStart);
+    BitBlt(hdc, 535, 135, 144, 107, hDCBitmap, 0, 0, SRCCOPY);
+  }
+  else
+  {
+    SelectObject(hDCBitmap, hBitmapReset);
+    BitBlt(hdc, 535, 135, 144, 107, hDCBitmap, 0, 0, SRCCOPY);
+  }
+  DeleteDC(hDCBitmap);
+}
+
 wynikGry CheckResult()
 {
   for (int i = 0; i < 3; i++)
@@ -220,28 +252,41 @@ wynikGry CheckResult()
 
 void Result(HWND hwndDig, wynikGry a)
 {
-  HWND hwndStatic2 = GetDlgItem(hwndDig, IDC_STATIC2);
   switch (a)
   {
   case wygrywaKolko:
-    wsprintf(Text, "Wygrywa kolko");
-    SetWindowText(hwndStatic2, Text);
-    isGameOn = false;
+    wsprintf(szText, "Wygrywa kolko");
+    //isGameOn = false;
+    MessageBox(hwndDig, szText, TEXT("Rezultat"), MB_OK);
     break;
   case wygrywaKrzyzyk:
-    wsprintf(Text, "Wygrywa krzyzyk");
-    SetWindowText(hwndStatic2, Text);
-    isGameOn = false;
+    wsprintf(szText, "Wygrywa krzyzyk");
+    //isGameOn = false;
+    MessageBox(hwndDig, szText, TEXT("Rezultat"), MB_OK);
     break;
   case remis:
-    wsprintf(Text, "Remis");
-    SetWindowText(hwndStatic2, Text);
-    isGameOn = false;
+    wsprintf(szText, "Remis");
+    //isGameOn = false;
+    MessageBox(hwndDig, szText, TEXT("Rezultat"), MB_OK);
     break;
   case nierozstrzygniety:;
   default:;
   }
-}*/
+}
 
-
-//void drawBitmap(HBITMAP hbmp, HDC hdc, int x, int y, int width, int height);
+void WstawPrzyciski(HWND hwndMainWindow)
+{
+  HDC hdcNowy = CreateCompatibleDC(Okno);
+  HDC hdc = GetDC(hwndMainWindow);
+  HBITMAP hbmOld = (HBITMAP)SelectObject(hdcNowy, hBitmapStart);
+  BITMAP bmInfo;
+  hdcNowy = CreateCompatibleDC(Okno);
+  hdc = GetDC(hwndMainWindow);
+  SelectObject(hdcNowy, hBitmapStart);
+  hbmOld = (HBITMAP)SelectObject(hdcNowy, hBitmapStart);
+  GetObject(hBitmapStart, sizeof(bmInfo), &bmInfo);
+  BitBlt(hdc, 535, 135, 144, 107, hdcNowy, 0, 0, SRCCOPY);
+  ReleaseDC(hwndMainWindow, hdc);
+  SelectObject(hdcNowy, hbmOld);
+  DeleteDC(hdcNowy);
+}
